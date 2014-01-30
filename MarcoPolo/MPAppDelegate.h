51a58b514321005784dd47848be5d3d38236d327
Erik Stromlund <erik@erikstromlund.com>
2014-01-29 22:43:41 -0800

    Initial Commit

---
+++ MarcoPolo/MPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MPAppDelegate.h
+//  MarcoPolo
+//
+//  Created by Erik Stromlund on 1/29/14.
+//  Copyright (c) 2014 FathomWorks LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MarcoPolo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MarcoPolo
+//
+//  Created by Erik Stromlund on 1/29/14.
+//  Copyright (c) 2014 FathomWorks LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MPAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MPAppDelegate class]));
+    }
+}
